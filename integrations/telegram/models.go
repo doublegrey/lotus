92cf933567f9c702a79dcdfec4eda400991bc1d4
doublegrey <sergejkropinov@gmail.com>
2020-11-16 12:50:36 +0200

    add comments for structs

--- integrations/telegram/models.go
+++ integrations/telegram/models.go
@@ -2,12 +2,14 @@ package telegram
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
+// Settings struct
 type Settings struct {
 	Name  string `json:"name"`
 	Token string `json:"token" binding:"required"`
 	Users []User `json:"users"`
 }
 
+// User struct
 type User struct {
 	Username string `json:"username"`
 	Chat     int    `json:"chat"`
